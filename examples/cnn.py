fix rotation bug

examples/cnn.py
@@ -157,63 +157,60 @@ class custom_CIFAR10(data.Dataset):
 
 import torch
 import torch.optim as optim
-import torchvision
-import torchvision.transforms as transforms
 
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-from ida_lib.core.pipeline_geometric_ops import TranslatePipeline, VflipPipeline, HflipPipeline, RandomShearPipeline
-from ida_lib.core.pipeline_pixel_ops import ContrastPipeline, NormalizePipeline
+from ida_lib.core.pipeline_geometric_ops import TranslatePipeline, VflipPipeline, HflipPipeline, RandomShearPipeline, \
+    RandomRotatePipeline
+from ida_lib.core.pipeline_pixel_ops import ContrastPipeline, NormalizePipeline, RandomContrastPipeline
 from ida_lib.image_augmentation.data_loader import AugmentDataLoader
 
 
 trainset = custom_CIFAR10(root='./data', train=True,
                                         download=True)
-#trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                          shuffle=True, num_workers=2)
+
+
 
 trainloader = AugmentDataLoader(dataset=trainset,
                   batch_size=4,
                   shuffle=True,
-                  pipeline_operations=(
-                      TranslatePipeline(probability=1, translation=(30, 10)),
-                      VflipPipeline(probability=0),
-                      HflipPipeline(probability=0),
-                      ContrastPipeline(probability=0, contrast_factor=1),
-                      RandomShearPipeline(probability=0, shear_range=(0, 0.5))),
-                  resize=(500, 500),
+                  pipeline_operations=(NormalizePipeline(probability=1),
+                        HflipPipeline(probability=1),
+                        RandomRotatePipeline(probability=0, degrees_range=(-15, 15)),
+                        RandomContrastPipeline(probability=0, contrast_range=(0.8, 1.2)),
+                        RandomShearPipeline(probability=0, shear_range=(0, 0.5))),
                   interpolation='bilinear',
                   padding_mode='zeros',
-                  output_format='tuple'
+                  output_format='tuple',
+                  output_type=torch.float32
                   )
 
 testset = custom_CIFAR10(root='./data', train=False,
                                        download=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+testloader = AugmentDataLoader(dataset=testset,
+                  batch_size=4,
+                  shuffle=False,
+                  pipeline_operations=(NormalizePipeline(probability=1),
+                        HflipPipeline(probability=0.5),
+                        RandomRotatePipeline(probability=0.8, degrees_range=(-15, 15)),
+                        RandomContrastPipeline(probability=0, contrast_range=(0.8, 1.2)),
+                        RandomShearPipeline(probability=0, shear_range=(0, 0.5))),
+                  interpolation='bilinear',
+                  padding_mode='zeros',
+                  output_format='tuple',
+                  output_type=torch.float32
+                  )
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+PATH = './cifar_net2.pth'
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-# functions to show an image
 
-def plot_dict_batch(batch):
-    batch_size = batch['image'].shape[0]
-    images = [item['image'][i, ...].cpu() for i in range(item[0]['image'].shape[0])]
-    labels = [item['target'][i, ...].cpu() for i in range(item[0]['target'].shape[0])]
-
-    fig, axs = plt.subplots(1, batch_size, figsize=(16, 10))
-    for i in range(batch_size):
-        axs[i].axis('off')
-        axs[i].set_title(classes[labels[i].item()])
-        img: np.ndarray = kornia.tensor_to_image(images[i].byte())
-        axs[i].imshow(img)
-    plt.show()
 
 def plot_tuple_batch(images, labels):
     batch_size = images.shape[0]
@@ -224,7 +221,7 @@ def plot_tuple_batch(images, labels):
     for i in range(batch_size):
         axs[i].axis('off')
         axs[i].set_title(classes[labels[i].item()])
-        img: np.ndarray = kornia.tensor_to_image(images[i].byte())
+        img: np.ndarray = kornia.tensor_to_image((images[i] * 255).byte())
         axs[i].imshow(img)
     plt.show()
 
@@ -257,50 +254,76 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
-#net = net.cuda()
+def train():
+    net = Net()
+    net = net.cuda()
 
-#Configure parameters
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    #Configure parameters
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-#TRAIN
-for epoch in range(2):  # loop over the dataset multiple times
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-        #labels = labels.to('cuda')
-        inputs = inputs.cpu()
-        # zero the parameter gradients
-        optimizer.zero_grad()
+    #TRAIN
+    from time import time
+    start_time = time()
+    for epoch in range(1):  # loop over the dataset multiple times
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs = (inputs.float())
+            labels = labels.to('cuda')
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            # forward + backward + optimize
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-
-print('Finished Training')
-
-
-PATH = './cifar_net.pth'
-torch.save(net.state_dict(), PATH)
-
-dataiter = iter(testloader)
-item = dataiter.next()
-
-# print images
-#plot_train_images(item)
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                      (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
+    consumed_time = time() - start_time
+    print(consumed_time)
+    print('Finished Training')
+    torch.save(net.state_dict(), PATH)
 
 
-net = Net()
-net.load_state_dict(torch.load(PATH))
+
+
+def test():
+    images, labels = dataiter.next()
+
+    # print images
+    plot_tuple_batch(images, labels)
+    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    net = Net()
+    net = net.cuda()
+    net.load_state_dict(torch.load(PATH))
+    outputs = net(images)
+    _, predicted = torch.max(outputs, 1)
+
+    print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                  for j in range(4)))
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            labels = labels.to('cuda')
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+
+train()

ida_lib/core/pipeline.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Optional
 import cv2
 import torch
 from ida_lib.operations.geometry_ops_functional import own_affine
@@ -36,7 +36,7 @@ class pipeline(object):
     """
 
     def __init__(self, pipeline_operations: list, resize: tuple = None, interpolation: str = 'bilinear',
-                 padding_mode: str = 'zeros', output_format: str = 'dict'):
+                 padding_mode: str = 'zeros', output_format: str = 'dict', output_type: Optional[torch.dtype] = torch.float32):
         """
         :param pipeline_operations: list of pipeline initialized operations (see pipeline_operations.py)
         :param resize: tuple of desired output size. Example (25,25)
@@ -46,6 +46,8 @@ class pipeline(object):
                 'zeros' | 'border' | 'reflection'. Default: 'zeros'
         :param output_format: desired format for each output item in the pipeline
                 'dict' (each item accompanied by its type name) | 'tuple'
+        :param: output_type: desired type for the bidimensional oputput items. If it is None, output type will be the same as input's type
+
         """
         self.color_ops, self.geom_ops, self.indep_ops = split_operations_by_type(pipeline_operations)
         self.geom_ops.reverse()  # to apply matrix multiplication in the user order
@@ -54,6 +56,7 @@ class pipeline(object):
         self.interpolation = interpolation
         self.padding_mode = padding_mode
         self.output_format = output_format
+        self.output_type = output_type
 
     def _apply_geometry_transform_data2d(self, image: torch.tensor, matrix: torch.tensor) -> torch.tensor:
         """
@@ -103,7 +106,8 @@ class pipeline(object):
             original = [d.copy() for d in batch_data]  # copy the original batch to diplay on visualization
         self.process_data = []
         principal_type = get_principal_type(batch_data[0])
-        original_type = dtype_to_torch_type(batch_data[0][principal_type].dtype)
+        if self.output_type is None:
+            self.output_type = dtype_to_torch_type(batch_data[0][principal_type].dtype)
         for index, data in enumerate(batch_data):
             if 'image' in data:
                 lut = get_compose_function(self.color_ops)
@@ -122,6 +126,6 @@ class pipeline(object):
             if self.info_data['contains_keypoints']: p_data['points_matrix'] = self._apply_geometry_transform_points(
                 p_data['points_matrix'], matrix)
             self.process_data.append(p_data)
-        return postprocess_data(batch=self.process_data, batch_info=self.info_data, data_original=original,  visualize=visualize, original_type = original_type, output_format = self.output_format)
+        return postprocess_data(batch=self.process_data, batch_info=self.info_data, data_original=original,  visualize=visualize, original_type = self.output_type, output_format = self.output_format)
 
 

ida_lib/core/pipeline_functional.py
@@ -87,11 +87,12 @@ def get_compose_function(operations: list) -> np.ndarray:
     """
     funcs = [op.transform_function for op in operations if op.apply_according_to_probability()]
     compose_function = functools.reduce(lambda f, g: lambda x: f(g(x)), tuple(funcs), lambda x: x)
-    lookUpTable = np.empty((1, 256), np.int16)
+    lookUpTable = np.empty((1, 256), np.float)
     for i in range(256):
         lookUpTable[0, i] = compose_function(i)
     lookUpTable[0, :] = np.clip(lookUpTable[0, :], 0, 255)
-    return np.uint8(lookUpTable)
+    return lookUpTable
+   # return np.uint8(lookUpTable)
 
 
 def preprocess_data(data:list, batch_info: dict=None,  interpolation: str=None, resize: Optional[tuple]=None) ->list:

ida_lib/core/pipeline_geometric_ops.py
@@ -3,6 +3,7 @@ import kornia
 import torch
 
 from ida_lib.core.pipeline_operations import PipelineOperation
+from ida_lib.operations.utils import round_torch
 
 device = 'cuda'
 cuda = torch.device('cuda')
@@ -135,6 +136,7 @@ class RotatePipeline(PipelineOperation):
         self.degrees = degrees
         self.degrees = degrees * one_torch
         self.new_row = torch.Tensor(1, 3).to(device)
+        self.new_row[:, 2] = 1
         if center is None:
             self.config = True
             self.center = ones_torch.clone()
@@ -172,6 +174,7 @@ class RandomRotatePipeline(PipelineOperation):
             raise Exception("Degrees range must be a tuple (min, max)")
         self.degrees_range = degrees_range
         self.new_row = torch.Tensor(1, 3).to(device)
+        self.new_row[:, 2] = 1
         if center is None:
             self.config = True
             self.center = ones_torch.clone()
@@ -185,7 +188,7 @@ class RandomRotatePipeline(PipelineOperation):
         if self.center_desviation is not None:
             center += random.randint(-self.center_desviation, self.center_desviation)
         return torch.cat(((kornia.geometry.get_rotation_matrix2d(angle=degrees.resize_(1), center=center,
-                                                                 scale=one_torch)).reshape(2, 3), self.new_row))
+                                                                 scale=one_torch.reshape(1))).reshape(2, 3), self.new_row))
 
     def need_data_info(self) -> bool:
         return self.config

ida_lib/image_augmentation/data_loader.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+import torch
 from torch.utils.data import Dataset, DataLoader
 
 from ida_lib.core.pipeline import pipeline
@@ -42,6 +45,7 @@ class AugmentDataLoader(DataLoader):
                  interpolation: str = 'bilinear',
                  padding_mode: str = 'zeros',
                  output_format: str = 'dict',
+                 output_type: Optional[torch.dtype] = None,
                  *args,
                  **kwargs):
 
@@ -51,7 +55,8 @@ class AugmentDataLoader(DataLoader):
                                      interpolation=interpolation,
                                      padding_mode=padding_mode,
                                      pipeline_operations=pipeline_operations,
-                                     output_format=output_format)
+                                     output_format=output_format,
+                                     output_type=output_type)
         else:
             self.pipeline = None
         self.dataset = self.__inner_Dataset(pipeline=self.pipeline, dataset=dataset)