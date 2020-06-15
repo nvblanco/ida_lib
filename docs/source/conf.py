08f1ab75c0365006482c4d7302bf36726339c814
raquel <raquelvilas18@hotmail.com>
2020-06-15 16:37:46 +0200

    requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,22 +24,6 @@ html_theme_options = {
     # If True, show hidden TOC entries
     "globaltoc_includehidden": False,
 }
-# Register the theme as an extension to generate a sitemap.xml
-
-
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
@@ -63,7 +47,6 @@ extensions = [
     'sphinx.ext.autodoc',
     "sphinx_rtd_theme",
 ]
-extensions.append("guzzle_sphinx_theme")
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -77,16 +60,6 @@ numpydoc_show_class_members = False
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#html_theme = 'alabaster'
-#html_theme = 'bootstrap'
-# html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 
