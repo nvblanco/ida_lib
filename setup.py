cafab7063f2d4732a8aa8091b79b4d8e714c2024
nicolas.vila.blanco <nicolas.vila.blanco@gmail.com>
2020-09-21 09:28:53 +0200

    Remove unwanted files

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ida_lib',
     version='1.0',
     description='Image Data Augmentation library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/raquelvilas18/ida_lib',
     author='Raquel Vilas',
     author_email='raquel.rodriguez.vilas@rai.usc.es',
-    packages=find_packages(exclude=["tests"]),
+    packages=find_packages(where="ida_lib", exclude=["tests"]),
     python_requires=">=3.5",
-    keywords="pytorch image data augmentation",
-    install_requires = ["opencv-python",
-                        "numpy>=1.18.4",
-                        "bokeh>=2.0.1",
-                        "kornia>=0.3.1",
-                        "torch>=1.5.0",
-                        "pandas",
-                        "tqdm"],
+    keywords=["Pytorch", "Image Data Augmentation"],
+    install_requires=["opencv-python",
+                      "numpy>=1.18.0",
+                      "bokeh>=2.0.0",
+                      "kornia>=0.3.1",
+                      "torch>=1.5.0",
+                      "pandas",
+                      "tqdm"],
     classifiers=[
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3',
         "Topic :: Software Development :: Libraries",
-         "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ]
 )
